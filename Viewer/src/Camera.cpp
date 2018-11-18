129c5ca513cdc9c86d0fa789acb3b54257955d19
asaf92 <asaf92@gmail.com>
2018-11-18 18:06:01 +0200

    Fixed lookAt (?)

--- Viewer/src/Camera.cpp
+++ Viewer/src/Camera.cpp
@@ -6,7 +6,7 @@
 
 
 Camera::Camera() : Camera::Camera(glm::vec4(0, 0, 1, 0),
-								  glm::vec4(0, 0, 0, 0),
+								  glm::vec4(0, 0, -1, 1),
 								  glm::vec4(0, 1, 0, 0))
 {
 
@@ -25,19 +25,25 @@ Camera::~Camera()
 
 void Camera::SetCameraLookAt(const glm::vec3& eye, const glm::vec3& at, const glm::vec3& up)
 {
-	glm::vec3 zAxis = glm::normalize(eye - at);
-	glm::vec3 xAxis = glm::normalize(glm::cross(zAxis,up));
-	glm::vec3 yAxis = cross(xAxis, zAxis);
+	glm::vec3 straight = glm::normalize(at - eye);
+	glm::vec3 upVector = glm::normalize(up);
+	glm::vec3 right = glm::normalize(glm::cross(straight, up));
+	upVector = glm::cross(straight, right);
 
-	zAxis = zAxis * -1.0f;
-
-	glm::mat4 viewMatrix = {
-		glm::vec4(xAxis.x,xAxis.y,xAxis.z,-glm::dot(xAxis,eye)),
-		glm::vec4(yAxis.x,yAxis.y,yAxis.z,-glm::dot(yAxis,eye)),
-		glm::vec4(zAxis.x,zAxis.y,zAxis.z,-glm::dot(zAxis,eye)),
-		glm::vec4(0,0,0,1)
-	};
-
+	glm::mat4x4 viewMatrix(1);
+	viewMatrix[0][0] = right.x;
+	viewMatrix[1][0] = right.y;
+	viewMatrix[2][0] = right.z;
+	viewMatrix[0][1] = upVector.x;
+	viewMatrix[1][1] = upVector.y;
+	viewMatrix[2][1] = upVector.z;
+	viewMatrix[0][2] = -straight.x;
+	viewMatrix[1][2] = -straight.y;
+	viewMatrix[2][2] = -straight.z;
+	viewMatrix[3][0] = -glm::dot(right, eye);
+	viewMatrix[3][1] = -glm::dot(upVector, eye);
+	viewMatrix[3][2] = glm::dot(straight, eye);
+	
 	viewTransformation = viewMatrix;
 }
 
