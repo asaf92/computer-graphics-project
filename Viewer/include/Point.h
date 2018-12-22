0fc9daf6e5a346be185eaae3466af7b593892d72
asaf92 <asaf92@gmail.com>
2018-12-22 17:27:01 +0200

    Added Z component to Point struct

--- Viewer/include/Point.h
+++ Viewer/include/Point.h
@@ -4,7 +4,8 @@ struct Point
 {
 	float X;
 	float Y;
+	float Z;
 
-	Point() : X(0), Y(0) {}
-	Point(float x, float y) :X(x), Y(y) {}
+	Point() : X(0.0f), Y(0.0f), Z(0.0f) {}
+	Point(float x, float y,float z) :X(x), Y(y),Z(z) {}
 };
