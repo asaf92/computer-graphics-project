b49aecf7cc584f0e575ac5281609f65df2e5c258
asaf92 <asaf92@gmail.com>
2019-01-04 14:34:02 +0200

    Removed redundant menu

--- Viewer/src/ImguiMenus.cpp
+++ Viewer/src/ImguiMenus.cpp
@@ -281,17 +281,6 @@ void ShowCameraControls(ImGuiIO& io, Scene& scene)
 	activeCamera.SetCameraLookAt(newLookAtParameters.eye, newLookAtParameters.at, newLookAtParameters.up);
 }
 
-void moveObjectControls(IMovable & movableObject, std::string title)
-{
-	static float moveRadius = 3.0f;
-	glm::vec3 moveDirection = glm::vec3(0);
-	ImGui::Text(title.c_str());
-	ImGui::SliderFloat("Move X", &moveDirection.x, -moveRadius, moveRadius);
-	ImGui::SliderFloat("Move Y", &moveDirection.y, -moveRadius, moveRadius);
-	ImGui::SliderFloat("Move Z", &moveDirection.z, -moveRadius, moveRadius);
-	movableObject.Move(moveDirection);
-}
-
 void ShowModelControls(ImGuiIO& io, Scene& scene)
 {
 	static float scalingSizesLimit = 5.0f;
@@ -543,6 +532,7 @@ void ShowLightsControls(ImGuiIO& io, Scene& scene)
 	ImGui::Text("Active light number: %d", selectedLightIndex + 1);
 }
 
+#pragma region Common controls
 void xyzSliders(glm::vec4 &newVector, std::string title, float radius)
 {
 	ImGui::Text(title.c_str());
@@ -551,73 +541,14 @@ void xyzSliders(glm::vec4 &newVector, std::string title, float radius)
 	ImGui::SliderFloat((std::string("Z ") +title).c_str(), &newVector.z, -radius, radius);
 }
 
-void DrawImguiMenus(ImGuiIO& io, Scene& scene)
+void moveObjectControls(IMovable & movableObject, const std::string title)
 {
-	// 1. Show the big demo window (Most of the sample code is in ImGui::ShowDemoWindow()! You can browse its code to learn more about Dear ImGui!).
-	if (showDemoWindow)
-	{
-		ImGui::ShowDemoWindow(&showDemoWindow);
-	}
-
-	// 2. Show a simple window that we create ourselves. We use a Begin/End pair to created a named window.
-	{
-		static float f = 0.0f;
-		static int counter = 0;
-
-		ImGui::Begin("Hello, world!");                          // Create a window called "Hello, world!" and append into it.
-
-		ImGui::Text("This is some useful text.");               // Display some text (you can use a format strings too)
-		ImGui::Checkbox("Demo Window", &showDemoWindow);      // Edit bools storing our window open/close state
-		ImGui::Checkbox("Another Window", &showAnotherWindow);
-
-		ImGui::SliderFloat("float", &f, 0.0f, 1.0f);            // Edit 1 float using a slider from 0.0f to 1.0f
-		ImGui::ColorEdit3("clear color", (float*)&clearColor); // Edit 3 floats representing a color
-
-		if (ImGui::Button("Button"))                            // Buttons return true when clicked (most widgets return true when edited/activated)
-			counter++;
-		ImGui::SameLine();
-		ImGui::Text("counter = %d", counter);
-
-		ImGui::Text("Application average %.3f ms/frame (%.1f FPS)", 1000.0f / ImGui::GetIO().Framerate, ImGui::GetIO().Framerate);
-		ImGui::End();
-	}
-
-	// 3. Show another simple window.
-	if (showAnotherWindow)
-	{
-		ImGui::Begin("Another Window", &showAnotherWindow);   // Pass a pointer to our bool variable (the window will have a closing button that will clear the bool when clicked)
-		ImGui::Text("Hello from another window!");
-		if (ImGui::Button("Close Me"))
-		{
-			showAnotherWindow = false;
-		}
-		ImGui::End();
-	}
-
-	// 4. Demonstrate creating a fullscreen menu bar and populating it.
-	{
-		ImGuiWindowFlags flags = ImGuiWindowFlags_NoFocusOnAppearing;
-		if (ImGui::BeginMainMenuBar())
-		{
-			if (ImGui::BeginMenu("File"))
-			{
-				if (ImGui::MenuItem("Load Model...", "CTRL+O"))
-				{
-					nfdchar_t *outPath = NULL;
-					nfdresult_t result = NFD_OpenDialog("obj;png,jpg", NULL, &outPath);
-					if (result == NFD_OKAY) {
-						scene.AddModel(std::make_shared<MeshModel>(Utils::LoadMeshModel(outPath)));
-						free(outPath);
-					}
-					else if (result == NFD_CANCEL) {
-					}
-					else {
-					}
-
-				}
-				ImGui::EndMenu();
-			}
-			ImGui::EndMainMenuBar();
-		}
-	}
+	static float moveRadius = 3.0f;
+	glm::vec3 moveDirection = glm::vec3(0);
+	ImGui::Text(title.c_str());
+	ImGui::SliderFloat("Move X", &moveDirection.x, -moveRadius, moveRadius);
+	ImGui::SliderFloat("Move Y", &moveDirection.y, -moveRadius, moveRadius);
+	ImGui::SliderFloat("Move Z", &moveDirection.z, -moveRadius, moveRadius);
+	movableObject.Move(moveDirection);
 }
+#pragma endregion
