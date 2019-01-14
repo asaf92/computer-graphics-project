26d909232def21e03addef2326f2e9c34fe8f6b0
asaf92 <asaf92@gmail.com>
2019-01-14 15:36:16 +0200

    Added Cube class

---
+++ Data/primitives/Cube.h
@@ -0,0 +1,22 @@
+#pragma once
+
+#include "MeshModel.h"
+
+class Cube : public IMeshObject, public IShaded
+{
+private:
+	float length;
+	float width;
+	float height;
+public:
+	Cube();
+	// IShaded
+	virtual const glm::mat4x4& GetWorldTransformation() { return glm::mat4x4(1); }
+	virtual const ShadingModels GetShadingMethod() { return Flat; };
+	virtual bool SetShadingMethod(ShadingModels model) { return false; };
+
+	// IMeshObject
+	virtual std::vector<glm::vec3>& GetVerticesVector();
+	virtual std::vector<Face>& GetFacesVector();
+	virtual const std::vector<glm::vec3>& GetNormalsVector() ;
+};
