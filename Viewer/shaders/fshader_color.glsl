e8d5e5f7ee63cac042b4599f2babc9c9b32979d3
asaf92 <asaf92@gmail.com>
2019-01-26 16:35:30 +0200

    Simplified shaders

--- Viewer/shaders/fshader_color.glsl
+++ Viewer/shaders/fshader_color.glsl
@@ -1,22 +1,22 @@
 #version 330 core
-
-struct Material
-{
-	sampler2D textureMap;
+//
+//struct Material
+//{
+//	sampler2D textureMap;
 	// You can add more fields here...
 	// Such as:
 	//		1. diffuse/specular relections constants
 	//		2. specular alpha constant
 	//		3. Anything else you find relevant
-};
+//};
 
 // We set this field's properties from the C++ code
-uniform Material material;
+//uniform Material material;
 
 // Inputs from vertex shader (after interpolation was applied)
-in vec3 fragPos;
-in vec3 fragNormal;
-in vec2 fragTexCoords;
+//in vec3 fragPos;
+//in vec3 fragNormal;
+//in vec2 fragTexCoords;
 
 // The final color of the fragment (pixel)
 out vec4 frag_color;
@@ -24,7 +24,7 @@ out vec4 frag_color;
 void main()
 {
 	// Sample the texture-map at the UV coordinates given by 'fragTexCoords'
-	vec3 textureColor = vec3(texture(material.textureMap, fragTexCoords));
+	// vec3 textureColor = vec3(texture(material.textureMap, fragTexCoords));
 
-	frag_color = vec4(textureColor, 1.0f);
+	frag_color = vec4(0.0f, 0.6f, 0.6f, 1.0f);
 }
