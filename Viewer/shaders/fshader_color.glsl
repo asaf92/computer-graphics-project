66a8ec24b8276b16f8ad7bb3bed3b03612de6178
asaf92 <asaf92@gmail.com>
2019-02-21 11:27:57 +0200

    Moved tune shading to the end of the fragment shading

--- Viewer/shaders/fshader_color.glsl
+++ Viewer/shaders/fshader_color.glsl
@@ -38,7 +38,7 @@ vec4 calculateSpecularPart(vec3 lightSourceLocation, vec4 lightColor);
 vec4 getDiffuseColor(float brightness, vec4 lightColor);
 float getBrightness(vec4 directionToLight, vec4 normalVector);
 float getDampedFactor(float relectionCameraDotProduct);
-vec4 capValuesAtOne(vec4 vector);
+vec4 toonShade(vec4 color);
 
 void main()
 {
@@ -50,19 +50,27 @@ void main()
 		diffusePartSum  = diffusePartSum  + calculateDiffusePart(lightsPositions[i],lightColors[i]);
 		specularPartSum = specularPartSum + calculateSpecularPart(lightsPositions[i],lightColors[i]);
 	}
-	diffusePartSum = capValuesAtOne(diffusePartSum);
-	specularPartSum = capValuesAtOne(specularPartSum);
+	diffusePartSum = diffusePartSum;
+	specularPartSum = specularPartSum;
 
-	frag_color = capValuesAtOne(ambiantColor * ambiantLighting + diffusePartSum + specularPartSum);
+	frag_color = ambiantColor * ambiantLighting + diffusePartSum + specularPartSum;
+	frag_color.w = 1.0f;
+	if(useToonShading)
+	{
+		frag_color = toonShade(frag_color);
+	}
 }
 
-vec4 capValuesAtOne(vec4 vector)
+vec4 toonShade(vec4 color)
 {
-	vector.x = min (vector.x, 1.0f);
-	vector.y = min (vector.y, 1.0f);
-	vector.z = min (vector.z, 1.0f);
-	vector.w = min (vector.w, 1.0f);
-	return vector;
+	float level = floor(color.r * toonShadingLevels);
+	color.r = level/toonShadingLevels;
+	level = floor(color.g * toonShadingLevels);
+	color.g = level/toonShadingLevels;
+	level = floor(color.b * toonShadingLevels);
+	color.b = level/toonShadingLevels;
+
+	return color;
 }
 
 vec4 calculateDiffusePart(vec3 lightSourceLocation, vec4 lightColor)
@@ -78,11 +86,11 @@ vec4 calculateDiffusePart(vec3 lightSourceLocation, vec4 lightColor)
 float getBrightness(vec4 directionToLight, vec4 normalVector)
 {
 	float brightness = dot(directionToLight, normalVector);
-	if(useToonShading)
-	{
-		float level = floor(brightness * toonShadingLevels);
-		brightness = level/toonShadingLevels;
-	}
+	//if(useToonShading)
+	//{
+	//	float level = floor(brightness * toonShadingLevels);
+	//	brightness = level/toonShadingLevels;
+	//}
 	return brightness;
 }
 
@@ -91,15 +99,15 @@ vec4 getDiffuseColor(float brightness, vec4 lightColor)
 	if(useTextures) 
 	{
 		vec4 color = vec4(texture(textureMap, fragTexCoords) * brightness * lightColor);
-		if(useToonShading)
-		{
-			float level = floor(color.r * toonShadingLevels);
-			color.r = level/toonShadingLevels;
-			level = floor(color.g * toonShadingLevels);
-			color.g = level/toonShadingLevels;
-			level = floor(color.b * toonShadingLevels);
-			color.b = level/toonShadingLevels;
-		}
+		//if(useToonShading)
+		//{
+		//	float level = floor(color.r * toonShadingLevels);
+		//	color.r = level/toonShadingLevels;
+		//	level = floor(color.g * toonShadingLevels);
+		//	color.g = level/toonShadingLevels;
+		//	level = floor(color.b * toonShadingLevels);
+		//	color.b = level/toonShadingLevels;
+		//}
 		return color;
 	}
 	return vec4(diffuseColor * brightness * lightColor);
@@ -132,24 +140,4 @@ float getDampedFactor(float relectionCameraDotProduct)
 		// do it
 	//}
 	return dampedFactor;
-}
-
-vec4 calculateToonSpecularPart(vec3 lightSourceLocation, vec4 lightColor)
-{
-	vec4 normalVector = normalize(vec4(fragNormal,0.0f));
-	vec4 directionToLight = vec4(normalize(lightSourceLocation - fragPosition),0.0f);
-	float normalFactor = dot(directionToLight,normalVector);
-	vec4 N = normalVector * normalFactor; // light source projection on the normal
-	vec4 P = vec4(N - vec4(lightSourceLocation,0.0f)); // Light source point to N
-	vec4 R = 2 * (dot(directionToLight, normalVector)) * normalVector - directionToLight;
-
-	vec4 V = vec4(normalize(cameraLocation - fragPosition),0.0f); // Direction to camera
-	float relectionCameraDotProduct = max(dot(R,V),0.0f);
-	float dampedFactor = pow(relectionCameraDotProduct,shininess);
-	dampedFactor = max(dampedFactor,0.001f);
-	float level = floor(dampedFactor * toonShadingLevels);
-	dampedFactor = level/toonShadingLevels;
-	vec4 result = specularColor * dampedFactor * lightColor; 
-	
-	return result;
 }
